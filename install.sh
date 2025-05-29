15fbb3130e6bcbf73d49775514f2c3804629b9e6
logicmagix <logicmagix@protonmail.com>
2025-05-29 17:37:11 -0400

    Allow install.sh to handle permissions

--- install.sh
+++ install.sh
@@ -1,21 +1,29 @@
 #!/usr/bin/env bash
 
+
 INSTALL_DIR="$HOME/.local/share/npalette"
 BIN_DIR="$HOME/.local/bin"
 WRAPPER="$BIN_DIR/npalette"
 
+
 mkdir -p "$INSTALL_DIR"
+mkdir -p "$BIN_DIR"
+
+
 cp -r npalette "$INSTALL_DIR"
 
-mkdir -p "$BIN_DIR"
-cat << EOF > "$WRAPPER"
+
+cat << 'EOF' > "$WRAPPER"
 #!/usr/bin/env bash
-PYTHONPATH="$INSTALL_DIR" python3 -m npalette.npalette
+PYTHONPATH="$HOME/.local/share/npalette" python3 -m npalette.npalette "$@"
 EOF
 
-chmod u+x "$WRAPPER"
 
-echo "npalette installed to $WRAPPER"
-echo "Make sure ~/.local/bin is in your PATH."
+chmod +x "$WRAPPER"
+
+
+echo "[+] npalette installed to: $WRAPPER"
+echo "[+] Make sure ~/.local/bin is in your PATH"
+
 
 
