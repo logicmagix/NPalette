610a7971413a28d2a42496e8f7c666bb343c610f
logicmagix <logicmagix@protonmail.com>
2025-08-13 18:02:51 -0400

    Fixed get_value error logic

--- npalette/utils/get_value.py
+++ npalette/utils/get_value.py
@@ -12,8 +12,8 @@ def get_value(prompt, lower=None, upper=None, convert_type=str):
         try:
             value = convert_type(value)
             if lower is not None and value <= lower:
-                if upper is not None and upper <= lower:
-                    error = f"Illegal argument: lower = {lower}. " \
+                if upper is not None and lower >= upper:
+                    error = f"Illegal argument: upper = {upper}. " \
                             f"Lower must be lower than {upper}."
                     print(error)
                     return None
@@ -21,9 +21,9 @@ def get_value(prompt, lower=None, upper=None, convert_type=str):
                         f"Entered value must be higher than {lower}."
                 print(error)
             elif upper is not None and value >= upper:
-                if lower is not None and upper <=lower:
+                if lower is not None and upper <= lower:
                     error = f"Illegal argument: upper = {upper}. " \
-                            f"Upper must be higher than {lower}."
+                            f"Upper must be higher than {lower}.."
                     print(error)
                     return None
                 error = f"Illegal argument: value = {value}. " \
@@ -33,7 +33,7 @@ def get_value(prompt, lower=None, upper=None, convert_type=str):
                 return value
         except ValueError:
             error = f"Illegal argument: value = {value}. " \
-                    f"Entered value must be of type:  \n {convert_type.__doc__}."
+                    f"Entered value must be of type:\n{convert_type.__doc__}"
             print(error)
 
 
